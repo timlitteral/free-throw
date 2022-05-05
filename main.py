c1a700bd532978171a4046843fd1c2fe6afc0058
timlitteral <46686893+timlitteral@users.noreply.github.com>
2022-05-04 18:31:11 -0700

    Added score counter, special effects, sounds, and animated taunt

--- main.py
+++ main.py
@@ -1,9 +1,88 @@
 @namespace
 class SpriteKind:
     Hoop = SpriteKind.create()
+    Stage = SpriteKind.create()
+
+def on_b_pressed():
+    animation.run_image_animation(mySprite,
+        [img("""
+                . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . f f f f f f . . . . . 
+                        . . . . . 4 4 4 4 4 4 . . . . . 
+                        . . . . . e f e e f e . . . . . 
+                        . . . . . e e e e e e . . . . . 
+                        . . . . . . . e e . . . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . 1 . 8 8 . 1 . . . . . 
+                        . . . . . e . 8 8 . e . . . . . 
+                        . . . . . . . 8 8 . . . . . . . 
+                        . . . . . . . 4 4 . . . . . . . 
+                        . . . . . . . . . . . . . . . .
+            """),
+            img("""
+                . . . . . . 5 5 5 5 . . . . . . 
+                        . . . . . . 5 5 5 5 . . . . . . 
+                        . . . . . . . 5 5 . . . . . . . 
+                        . . . . . f f f f f f . . . . . 
+                        . . . e . 4 4 4 4 4 4 . e . . . 
+                        . . . 1 . e f e e f e . 1 . . . 
+                        . . . e . e e e e e e . e . . . 
+                        . . . e . . . e e . . . e . . . 
+                        . . . e e e 8 8 8 8 e e e . . . 
+                        . . . . . . 8 8 8 8 . . . . . . 
+                        . . . . . . 8 8 8 8 . . . . . . 
+                        . . . . . . . 8 8 . . . . . . . 
+                        . . . . . . 8 8 8 8 . . . . . . 
+                        . . . . . 8 8 . . 8 8 . . . . . 
+                        . . . . 4 4 . . . . 4 4 . . . . 
+                        . . . . . . . . . . . . . . . .
+            """),
+            img("""
+                . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . f f f f f f . . . . . 
+                        . . . . . 4 4 4 4 4 4 . . . . . 
+                        . . . . . e f e e f e . . . . . 
+                        . . . . . e e e e e e . . . . . 
+                        . . . . . . . e e . . . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . 1 . 8 8 . 1 . . . . . 
+                        . . . . . e . 8 8 . e . . . . . 
+                        . . . . . . . 8 8 . . . . . . . 
+                        . . . . . . . 4 4 . . . . . . . 
+                        . . . . . . . . . . . . . . . .
+            """)],
+        800,
+        False)
+    if controller.B.is_pressed():
+        mySprite.say_text(":P", 500, True)
+        if controller.B.is_pressed():
+            music.play_melody("G F C G F C C5 C5 ", 274)
+controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
 
 def on_on_overlap(sprite, otherSprite):
-    game.over(True, effects.confetti)
+    mySprite2.start_effect(effects.halo, 200)
+    music.pew_pew.play()
+    projectile.destroy()
+    info.change_score_by(1)
+    if info.player1.score() > 55:
+        mySprite2.start_effect(effects.fire, 2000)
+        music.pew_pew.play()
+    if info.player1.score() > 75:
+        mySprite2.start_effect(effects.ashes, 5000)
+        music.zapped.play()
+    if info.player1.score() > 90:
+        mySprite2.start_effect(effects.disintegrate, 5000)
+        music.spooky.play()
+    if info.score() == 100:
+        game.over(True, effects.confetti)
 sprites.on_overlap(SpriteKind.projectile, SpriteKind.Hoop, on_on_overlap)
 
 def on_a_released():
@@ -11,9 +90,11 @@ def on_a_released():
     projectile = sprites.create_projectile_from_sprite(assets.image("""
         Basketball
     """), mySprite, 0, -100)
+    projectile.set_flag(SpriteFlag.DESTROY_ON_WALL, True)
 controller.A.on_event(ControllerButtonEvent.RELEASED, on_a_released)
 
 projectile: Sprite = None
+mySprite2: Sprite = None
 mySprite: Sprite = None
 scene.set_background_image(assets.image("""
     Half-court
--- main.ts
+++ main.ts
@@ -1,17 +1,104 @@
 namespace SpriteKind {
     export const Hoop = SpriteKind.create()
+    export const Stage = SpriteKind.create()
 }
 
+controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
+    animation.runImageAnimation(mySprite, [img`
+                . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . f f f f f f . . . . . 
+                        . . . . . 4 4 4 4 4 4 . . . . . 
+                        . . . . . e f e e f e . . . . . 
+                        . . . . . e e e e e e . . . . . 
+                        . . . . . . . e e . . . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . 1 . 8 8 . 1 . . . . . 
+                        . . . . . e . 8 8 . e . . . . . 
+                        . . . . . . . 8 8 . . . . . . . 
+                        . . . . . . . 4 4 . . . . . . . 
+                        . . . . . . . . . . . . . . . .
+            `, img`
+                . . . . . . 5 5 5 5 . . . . . . 
+                        . . . . . . 5 5 5 5 . . . . . . 
+                        . . . . . . . 5 5 . . . . . . . 
+                        . . . . . f f f f f f . . . . . 
+                        . . . e . 4 4 4 4 4 4 . e . . . 
+                        . . . 1 . e f e e f e . 1 . . . 
+                        . . . e . e e e e e e . e . . . 
+                        . . . e . . . e e . . . e . . . 
+                        . . . e e e 8 8 8 8 e e e . . . 
+                        . . . . . . 8 8 8 8 . . . . . . 
+                        . . . . . . 8 8 8 8 . . . . . . 
+                        . . . . . . . 8 8 . . . . . . . 
+                        . . . . . . 8 8 8 8 . . . . . . 
+                        . . . . . 8 8 . . 8 8 . . . . . 
+                        . . . . 4 4 . . . . 4 4 . . . . 
+                        . . . . . . . . . . . . . . . .
+            `, img`
+                . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . f f f f f f . . . . . 
+                        . . . . . 4 4 4 4 4 4 . . . . . 
+                        . . . . . e f e e f e . . . . . 
+                        . . . . . e e e e e e . . . . . 
+                        . . . . . . . e e . . . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . e 8 8 8 8 e . . . . . 
+                        . . . . . 1 . 8 8 . 1 . . . . . 
+                        . . . . . e . 8 8 . e . . . . . 
+                        . . . . . . . 8 8 . . . . . . . 
+                        . . . . . . . 4 4 . . . . . . . 
+                        . . . . . . . . . . . . . . . .
+            `], 800, false)
+    if (controller.B.isPressed()) {
+        mySprite.sayText(":P", 500, true)
+        if (controller.B.isPressed()) {
+            music.playMelody("G F C G F C C5 C5 ", 274)
+        }
+        
+    }
+    
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Hoop, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
-    game.over(true, effects.confetti)
+    mySprite2.startEffect(effects.halo, 200)
+    music.pewPew.play()
+    projectile.destroy()
+    info.changeScoreBy(1)
+    if (info.player1.score() > 55) {
+        mySprite2.startEffect(effects.fire, 2000)
+        music.pewPew.play()
+    }
+    
+    if (info.player1.score() > 75) {
+        mySprite2.startEffect(effects.ashes, 5000)
+        music.zapped.play()
+    }
+    
+    if (info.player1.score() > 90) {
+        mySprite2.startEffect(effects.disintegrate, 5000)
+        music.spooky.play()
+    }
+    
+    if (info.score() == 100) {
+        game.over(true, effects.confetti)
+    }
+    
 })
 controller.A.onEvent(ControllerButtonEvent.Released, function on_a_released() {
     
     projectile = sprites.createProjectileFromSprite(assets.image`
         Basketball
     `, mySprite, 0, -100)
+    projectile.setFlag(SpriteFlag.DestroyOnWall, true)
 })
 let projectile : Sprite = null
+let mySprite2 : Sprite = null
 let mySprite : Sprite = null
 scene.setBackgroundImage(assets.image`
     Half-court
@@ -22,7 +109,7 @@ mySprite = sprites.create(assets.image`
 mySprite.setVelocity(50, 0)
 mySprite.setPosition(77, 62)
 mySprite.setBounceOnWall(true)
-let mySprite2 = sprites.create(assets.image`
+mySprite2 = sprites.create(assets.image`
     Hoop
 `, SpriteKind.Hoop)
 mySprite2.setPosition(77, 9)
